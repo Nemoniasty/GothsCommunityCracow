32e5b4d3e5084786102bb5a1a4435edd27bae067
Nemoniasty <145905629+Nemoniasty@users.noreply.github.com>
2025-05-20 06:53:50 +0000

    [FIX] Forgot about building the project

--- GothsCommunityCracow/src/Header.jsx
+++ GothsCommunityCracow/src/Header.jsx
@@ -1,12 +1,14 @@
 
 
-function Header() {
+function Header({name="no name"}) {
     
     return(
         <>
             <Header className="Header">
-
+                {name}
             </Header>
         </>
     )
 }
+
+export default Header
